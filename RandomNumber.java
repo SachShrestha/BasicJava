7e950c26e3533e221b19fc5442f12833e7ac2faa
SachShrestha <39011651+SachShrestha@users.noreply.github.com>
2018-07-14 11:26:00 -0500

    MathOperator

---
+++ RandomNumber.java
@@ -0,0 +1,23 @@
+package CoreJava;
+
+public class RandomNumber {
+
+	public static void main(String[] args) {
+
+	int i,s; {
+
+	for (i=1;i <=500;i++)
+
+	{
+
+	s=(int)Math.random ()*500;
+
+	System.out.println (s);
+
+	}
+	
+
+}
+	}
+	
+}
